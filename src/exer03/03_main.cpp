f0e38ba189b9714507bef2b1b6e41cf362fd2450
Lada-Rom <doodleztoon@gmail.com>
2023-06-05 11:58:47 +0300

    NEW: exer03 convert image to other color spaces

--- src/exer03/03_main.cpp
+++ src/exer03/03_main.cpp
@@ -5,11 +5,14 @@
 #include "quality.hpp"
 
 
-void ideal_solution(const cv::Mat& src, cv::Mat& dst) {
-  cv::cvtColor(src, dst, cv::COLOR_BGR2GRAY);
+void ideal_solution(const cv::Mat& src, std::vector<cv::Mat>& dst) {
+  dst.resize(3);
+  cv::cvtColor(src, dst[0], cv::COLOR_BGR2HSV);
+  cv::cvtColor(src, dst[1], cv::COLOR_BGR2HLS);
+  cv::cvtColor(src, dst[2], cv::COLOR_BGR2Lab);
 }
 
-void test_solution(const cv::Mat& src, cv::Mat& dst) {
+void test_solution(const cv::Mat& src, std::vector<cv::Mat>& dst) {
   // load student solution here instead of using ideal solution
   // cv::Mat dst = cv::imread("path/to/student/image/result.png");
   ideal_solution(src, dst);
@@ -18,10 +21,10 @@ void test_solution(const cv::Mat& src, cv::Mat& dst) {
 void main() {
   cv::Mat src = cv::imread("../../../data/src.01.png");
 
-  cv::Mat result{};
+  std::vector<cv::Mat> result{};
   test_solution(src, result);
 
-  cv::Mat ideal{};
+  std::vector<cv::Mat> ideal{};
   ideal_solution(src, ideal);
 
   measureQuality(src, ideal, result);
--- src/quality_assessment/quality.hpp
+++ src/quality_assessment/quality.hpp
@@ -7,7 +7,11 @@
 #include <opencv2/opencv.hpp>
 
 
-void measureQuality(const cv::Mat& src, const cv::Mat& ideal, const cv::Mat& result) {
+void measureQuality(
+  const cv::Mat& src,
+  const cv::Mat& ideal,
+  const cv::Mat& result) {
+
   auto make_gray = [](const cv::Mat& src, cv::Mat& dst) {
     if (src.channels() == 1)
       dst = src.clone();
@@ -31,6 +35,46 @@ void measureQuality(const cv::Mat& src, const cv::Mat& ideal, const cv::Mat& res
   cv::imshow("ideal solution", ideal);
   std::cout << std::fixed << std::setprecision(2)
             << "Quality: " << 1 - normalized_mae << std::endl;
+}
+
+
+void measureQuality(
+  const cv::Mat& src,
+  const std::vector<cv::Mat>& ideal,
+  const std::vector<cv::Mat>& result) {
+
+  auto make_gray = [](const cv::Mat& src, cv::Mat& dst) {
+    if (src.channels() == 1)
+      dst = src.clone();
+    else
+      cv::cvtColor(src, dst, cv::COLOR_BGR2GRAY);
+  };
+
+  if (ideal.size() != result.size()) {
+    std::cout << "Different number of ideal and result images!" << std::endl;
+    return;
+  }
+
+  for (size_t img_idx{}; img_idx < ideal.size(); ++img_idx) {
+    cv::Mat result_gray{};
+    make_gray(result[img_idx], result_gray);
+
+    cv::Mat ideal_gray{};
+    make_gray(ideal[img_idx], ideal_gray);
+
+    cv::Mat abs_error = cv::abs(ideal_gray - result_gray);
+    uint32_t abs_error_sum = cv::sum(abs_error)[0];
+    float mae = 1.0 * abs_error_sum / (ideal_gray.rows * ideal_gray.cols);
+    float normalized_mae = mae / 255;
+
+    cv::imshow("custom solution " + std::to_string(img_idx), result[img_idx]);
+    cv::imshow("ideal solution " + std::to_string(img_idx), ideal[img_idx]);
+    std::cout << std::fixed << std::setprecision(2)
+      << "Quality " << img_idx << ": "
+      << 1 - normalized_mae << std::endl;
+  }
+  cv::imshow("src image", src);
+
 }
 
 #endif //QUALITY_HPP
